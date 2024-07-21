c0707ca1035209dcae702df2c1773843d597b671
Amina880 <dkalekwa37@gmail.com>
2024-07-21 23:30:24 +0300

    added js

---
+++ quiz.js
@@ -0,0 +1,20 @@
+function checkAnswer() {
+    let correctAnswer = '4'
+    let checkedradiobutton = document.querySelector('input[name="quiz"]:checked')
+    let userAnswer = checkedradiobutton.value;
+    if(userAnswer === correctAnswer){
+        feedbackElement.textContent = "Correct! Well done."
+    }else{
+        feedbackElement.textContent = "That's incorrect. Try again!"
+    }
+    // Function body
+}
+let submit = document.getElementById('submit-answer')
+submit.addEventListener('click', checkAnswer);
+
+
+/*Compare the User’s Answer with the Correct Answer:
+
+Implement an if statement to compare userAnswer with correctAnswer.
+If the values match, indicating the user’s answer is correct, change the textContent of the element with the ID feedback to "Correct! Well done.".
+If the values do not match, indicating the user’s answer is incorrect, update the textContent of the feedback element to "That's incorrect. Try again!".*/
